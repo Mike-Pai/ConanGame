ec061f5d58111882dfca0069815c649d26277fce
MikePai <ye518612@gmail.com>
2021-03-22 15:47:42 +0800

    Initial Commit

---
+++ ConanGame/ConanGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConanGameApp.swift
+//  ConanGame
+//
+//  Created by 白謹瑜 on 2021/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ConanGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConanGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConanGame
+//
+//  Created by 白謹瑜 on 2021/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
